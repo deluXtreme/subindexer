4fe435f98e0118be09224f8665647da976f2b404
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-18 15:18:28 +0200

    Contract & Query Update (#8)
    Test Plan
    
    Wait an hour to see the amount change on the api server from 1000... to
    2000... or more.

--- src/redeemable-subscriptions.sql
+++ src/redeemable-subscriptions.sql
@@ -4,8 +4,10 @@ WITH
             active.id,
             active.subscriber,
             recipient,
-            amount,
-            category
+            amount::NUMERIC as amount,
+            category,
+            frequency::NUMERIC as frequency,
+            creation_timestamp::NUMERIC as creation_timestamp
         FROM subindexer_subscription_module.subscription_created active
                  LEFT JOIN subindexer_subscription_module.unsubscribed canceled
                            ON active.id = canceled.id
@@ -14,7 +16,8 @@ WITH
     latest_redemptions AS (
         SELECT DISTINCT ON (id)
             id,
-            next_redeem_at
+            next_redeem_at,
+            block_number as last_redeemed
         FROM subindexer_subscription_module.redeemed
         ORDER BY id, next_redeem_at DESC
     ),
@@ -24,17 +27,21 @@ WITH
             new_recipient
         FROM subindexer_subscription_module.recipient_updated
         ORDER BY id, block_number DESC
+    ),
+    upcomming AS (
+        SELECT
+            a.id,
+            a.subscriber,
+            COALESCE(rp.new_recipient, a.recipient) AS recipient,
+            -- Redeemable Amount: cf https://github.com/deluXtreme/subi-contracts/blob/65455f02e3e7a49654c51b9b5e805cccc1032168/src/SubscriptionModule.sol#L154-L158
+            (FLOOR((FLOOR(EXTRACT(EPOCH FROM now()))::NUMERIC - COALESCE(cast(r.last_redeemed as Integer), creation_timestamp)) / a.frequency) * a.amount)::TEXT as amount,
+            category,
+            COALESCE(cast(r.next_redeem_at as Integer), creation_timestamp) AS next_redeem_at
+        FROM active_subscriptions a
+                LEFT JOIN latest_redemptions r
+                        ON a.id = r.id
+                LEFT JOIN latest_recipients rp
+                        ON a.id = rp.id
     )
-SELECT
-    a.id,
-    a.subscriber,
-    COALESCE(rp.new_recipient, a.recipient) AS recipient,
-    a.amount,
-    category,
-    COALESCE(cast(r.next_redeem_at as Integer), 0) AS next_redeem_at
-FROM active_subscriptions a
-         LEFT JOIN latest_redemptions r
-                   ON a.id = r.id
-         LEFT JOIN latest_recipients rp
-                   ON a.id = rp.id
-WHERE COALESCE(cast(r.next_redeem_at as Integer), 0) < $1;
+SELECT * from upcomming
+WHERE next_redeem_at < $1;
