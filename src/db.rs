dd2b4af27b09f66e135eb534458039a2e10ed652
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-20 19:31:24 +0200

    Bake file into image

--- src/db.rs
+++ src/db.rs
@@ -1,18 +1,13 @@
 use crate::models::RedeemableSubscription;
 use sqlx::PgPool;
-use tokio::fs;
 
-const QUERY_PATH: &str = "src/redeemable-subscriptions.sql";
+const REDEEMABLE_QUERY: &str = include_str!("redeemable-subscriptions.sql");
 
 pub async fn get_redeemable_subscriptions(
     pool: &PgPool,
     current_timestamp: i32,
 ) -> Result<Vec<RedeemableSubscription>, sqlx::Error> {
-    let query = fs::read_to_string(QUERY_PATH)
-        .await
-        .unwrap_or_else(|e| panic!("Failed to read SQL file at '{QUERY_PATH}': {e}"));
-
-    sqlx::query_as::<_, RedeemableSubscription>(&query)
+    sqlx::query_as::<_, RedeemableSubscription>(REDEEMABLE_QUERY)
         .bind(current_timestamp)
         .fetch_all(pool)
         .await
