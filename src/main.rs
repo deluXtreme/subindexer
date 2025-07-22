44984ca751ee0f010d0fe69a5fdd654423a408d9
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-22 10:53:17 +0200

    Prod Compose (#14)
    We had a lot of config setup. Thought it would be better to load that
    all in one place and pass it around.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod api;
+mod config;
 mod db;
 mod models;
 mod redeem;
 
-use alloy::signers::local::PrivateKeySigner;
 use axum::{Router, routing::get};
+use config::Config;
 use dotenv::dotenv;
-use sqlx::{PgPool, postgres::PgPoolOptions};
-use std::{env, net::SocketAddr};
+use std::net::SocketAddr;
 use tokio::time::{Duration, interval};
 use tower_http::cors::{Any, CorsLayer};
 
@@ -19,13 +19,8 @@ async fn main() {
     // Initialize tracing
     tracing_subscriber::fmt::init();
 
-    // Database connection
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = PgPoolOptions::new()
-        .max_connections(2)
-        .connect(&database_url)
-        .await
-        .expect("Failed to connect to Postgres");
+    let config = Config::from_env().await;
+    config.log_non_secrets();
 
     // Build our application with a route
     let app = Router::new()
@@ -37,51 +32,24 @@ async fn main() {
                 .allow_methods(Any)
                 .allow_headers(Any),
         )
-        .with_state(pool.clone());
+        .with_state(config.pool.clone());
 
-    // Get port from environment variable or default to 3000
-    let port = std::env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string())
-        .parse::<u16>()
-        .expect("PORT must be a number");
-
-    let redeem_interval = std::env::var("REDEEM_INTERVAL")
-        .unwrap_or_else(|_| "3600".to_string())
-        .parse::<u64>()
-        .expect("REDEEM_INTERVAL must be a number");
-
-    // Run it
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let addr = SocketAddr::from(([0, 0, 0, 0], config.api_port));
     tracing::info!("listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    let signer: PrivateKeySigner = env::var("PK")
-        .expect("PK must be set")
-        .parse()
-        .expect("Invalid Signer Key");
-    tracing::info!("redeemer account {}", signer.address());
-    let rpc_url =
-        env::var("GNOSIS_RPC_URL").unwrap_or_else(|_| "https://rpc.gnosischain.com/".to_string());
-    tokio::spawn(spawn_redeemer(
-        redeem_interval,
-        rpc_url,
-        pool.clone(),
-        signer,
-    ));
+    tokio::spawn(spawn_redeemer(config));
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn spawn_redeemer(
-    redeem_interval: u64,
-    rpc_url: String,
-    pool: PgPool,
-    signer: PrivateKeySigner,
-) {
-    let mut ticker = interval(Duration::from_secs(redeem_interval));
+async fn spawn_redeemer(config: Config) {
+    let mut ticker = interval(Duration::from_secs(config.redeem_interval));
     tracing::info!("Cron redeemer");
     loop {
         ticker.tick().await;
-        if let Err(err) = redeem::run_redeem_job(&rpc_url, &pool, &signer).await {
+        if let Err(err) =
+            redeem::run_redeem_job(&config.rpc_url, &config.pool, &config.redeemer).await
+        {
             tracing::error!("Redeem job failed: {err:?}");
         }
     }
